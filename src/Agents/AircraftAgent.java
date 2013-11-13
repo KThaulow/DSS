b725a1fdb9cc4c57838f704eb2c25e6f065459e5
barto90 <kristian1990@gmail.com>
2013-11-13 12:59:44 +0100

    RescheduleRequestsServer behaviour added

--- src/Agents/AircraftAgent.java
+++ src/Agents/AircraftAgent.java
@@ -1,11 +1,13 @@
 package Agents;
 
-
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class AircraftAgent extends Agent {
 
@@ -15,7 +17,8 @@ public class AircraftAgent extends Agent {
     protected void setup() {
         registerToDF();
         System.out.println("Hello, this is an aircraft agent!");
-        //addBehaviour(new SomeBehaviour());
+        
+        addBehaviour(new RescheduleRequestsServer()); // Serve the reschedule request
     }
 
     private void registerToDF() {
@@ -42,5 +45,33 @@ public class AircraftAgent extends Agent {
         }
 
         System.out.println("Plane agent " + getAID().getName() + " terminating");
+    }
+
+    private class RescheduleRequestsServer extends CyclicBehaviour {
+
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                // Message received. Process it
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+
+                String response = "";
+                
+                /**
+                 * response = ...
+                 * Calculate cost
+                 * Get capacity of aircraft
+                 */
+                
+                reply.setPerformative(ACLMessage.PROPOSE);
+                reply.setContent(response);
+
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
     }
 }
