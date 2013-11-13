47aa19ae626fd9720b1cb00fee7535ca39eedd9e
barto90 <kristian1990@gmail.com>
2013-11-13 08:48:20 +0100

    Airport and plane agent added

---
+++ src/PlaneAgent.java
@@ -0,0 +1,44 @@
+
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+public class PlaneAgent extends Agent {
+
+    private static final String typeOfAgent = "plane";
+    private static final String nameOfAgent = "planeAgent";
+
+    protected void setup() {
+        registerToDF();
+
+        //addBehaviour(new SomeBehaviour());
+    }
+
+    private void registerToDF() {
+        // Register the plane service in the yellow pages
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(typeOfAgent);
+        sd.setName(nameOfAgent);
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
+
+    protected void takeDown() {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        System.out.println("Plane agent " + getAID().getName() + " terminating");
+    }
+}
