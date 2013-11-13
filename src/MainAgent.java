a588430a236bf31b1209b64786158b412360d477
pelam09 <pelam09@student.sdu.dk>
2013-11-13 12:11:49 +0100

    - Further refraction - Agent creation is now delegated to a one-shot behaviour

--- src/MainAgent.java
+++ src/MainAgent.java
@@ -1,12 +1,6 @@
 
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
-import jade.lang.acl.ACLMessage;
-import jade.proto.AchieveREInitiator;
-import Utils.*;
-import jade.wrapper.ControllerException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import AgentBehaviours.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,67 +19,23 @@ public class MainAgent extends Agent
     protected void setup() 
     {
         System.out.println("Successfully initialized main agent.");
-        System.out.println(tryCreateAircraftAgents(NUMBER_OF_AGENTS_TO_CREATE) ? "Successfully created all" + NUMBER_OF_AGENTS_TO_CREATE+ " agents!" : "FAIL!"); // Test, do delete again.
+        
+        String containerName;
+        
+        try
+        {
+            containerName = getContainerController().getContainerName();
+        }
+        catch(Exception c)
+        {
+            containerName = "Main-Container";
+        }
+        
+        addBehaviour(new CreateGenericAgentBehaviour(NUMBER_OF_AGENTS_TO_CREATE, containerName, "Agents.AircraftAgent", "acAgent"));
     }
     
     protected void takeDown() 
     {
         System.out.println("shutting down main");
-    }
-    
-    private boolean tryCreateAircraftAgents(int numberOfAgents)
-    {
-        try
-        {
-            AgentCreationMsgFactory fact = new AgentCreationMsgFactory(getAMS(), getContentManager());
-
-            System.out.println("Creating " + numberOfAgents + " aircraft agents in the main container...");
-
-            String agentClassName = "Agents.AircraftAgent";
-            String containerName;
-
-            try 
-            {
-                containerName = getContainerController().getContainerName();
-            } 
-            catch (ControllerException ex) 
-            {
-                containerName = "Main-Container";
-            }
-
-            for(int agents = 0; agents < numberOfAgents; agents++)
-            {
-                String agentName = "aircraftAgent" + agents;
-                ACLMessage agentCreationMsg = fact.createRequestMessage(agentClassName, agentName, containerName);
-                addBehaviour(createAgentCreationBehaviour(agentCreationMsg, agentClassName, agentName, containerName));
-            }
-            return true;
-        }
-        catch(Exception c)
-        {
-            return false;
-        }
-    }
-    
-    private Behaviour createAgentCreationBehaviour(ACLMessage request, String agentClass, String agentName, String containerName)
-    {
-        final String ac = agentClass;
-        final String an = agentName;
-        final String cn = containerName;
-        
-        return new AchieveREInitiator(this, request)
-            {
-                protected void handleInform(ACLMessage inform) 
-                {
-                    System.out.println("Successfully created a '" + ac + "' named '" + an 
-                            + "' inside the following container: " + cn);
-                }
-
-                protected void handleFailure(ACLMessage failure) 
-                {
-                    System.out.println("Failed to create a '" + ac + "' named '" + an
-                            + "' inside the following container: " + cn);
-                }
-            };
     }
 }
