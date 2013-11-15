8d3501c54fd7fa0ee12dba91344ebd36a64cbc41
krist_000 <krist_000@Kristian-Yoga>
2013-11-15 14:36:27 +0100

    ArrivalAirportRequestServerBehaviour added in RouteAgent

--- src/Agents/AirportAgent.java
+++ src/Agents/AirportAgent.java
@@ -75,7 +75,7 @@ public class AirportAgent extends Agent {
 
         @Override
         public void action() {
-            MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId(airportLocationID), MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+            MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId(airportLocationConID), MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
 
             ACLMessage msg = myAgent.receive(mt);
             if (msg != null) {
@@ -84,7 +84,7 @@ public class AirportAgent extends Agent {
                 
                 String response = coordinateX + "," + coordinateY;
 
-                reply.setConversationId(airportLocationID);
+                reply.setConversationId(airportLocationConID);
                 reply.setPerformative(ACLMessage.INFORM);
                 reply.setContent(response);
 
